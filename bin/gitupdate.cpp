d8a1c0db111dc93d6adcbf68677261c9db22ba0b
千草颯 <SoChigusa@So-Chigusa.local>
2018-07-06 00:08:37 +0900

    auto commit by gitupdate

--- bin/gitupdate.cpp
+++ bin/gitupdate.cpp
@@ -9,7 +9,13 @@
 #include "html.h"
 
 int main(int argc, char** argv) {
+  std::string comment="";
   HTML::update_html("../git/index_temp.html", "../git/index.html",
 		    "../git/contents.html");
+  system("cp ../git/index.html ../../sochigusa.bitbucket.org/git/index.html");
+  system(("git commit -a -m \"auto commit by gitupdate"+comment+"\" && "
+  	  +"git push origin master").c_str());
+  system(("cd ../../sochigusa.bitbucket.org/ && git commit -a -m \"auto commit by gitupdate"+comment+"\" && "
+  	  +"git push origin master").c_str());
   return 0;
 }
