f3f19a873f423d6b2e004a4f08b4650137423852
千草颯 <SoChigusa@So-Chigusa.local>
2018-07-21 21:00:29 +0200

    auto commit by diarylog : update

--- bin/diarylog.cpp
+++ bin/diarylog.cpp
@@ -20,12 +20,12 @@ void errorMessage() {
 
 void update_log(const std::string &comment) {
   HTML::update_diarylog();
-  // system("cp ../diary/index.html ../../sochigusa.bitbucket.org/diary/index.html");
-  // system("cp ../diary/page-* ../../sochigusa.bitbucket.org/diary/");
-  // system(("git add ../diary/page-* && git commit -a -m \"auto commit by diarylog : "+comment+"\" && "
-  // 	  +"git push origin master").c_str());
-  // system(("cd ../../sochigusa.bitbucket.org/ && git add diary/page-* && git commit -a -m \"auto commit by diarylog : "+comment+"\" && "
-  // 	  +"git push origin master").c_str());
+  system("cp ../diary/index.html ../../sochigusa.bitbucket.org/diary/index.html");
+  system("cp ../diary/page-* ../../sochigusa.bitbucket.org/diary/");
+  system(("git add ../diary/page-* && git commit -a -m \"auto commit by diarylog : "+comment+"\" && "
+  	  +"git push origin master").c_str());
+  system(("cd ../../sochigusa.bitbucket.org/ && git add diary/page-* && git commit -a -m \"auto commit by diarylog : "+comment+"\" && "
+  	  +"git push origin master").c_str());
 }
 
 void add_log(std::string &arg_name, std::ifstream &ifcont) {
