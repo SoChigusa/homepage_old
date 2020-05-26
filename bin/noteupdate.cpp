f2a06166684841abd20023b03312771cb0b200d5
千草颯 <chigusa@hep-th.phys.s.u-tokyo.ac.jp>
2020-05-27 01:21:03 +0900

    Bug fixed

--- bin/noteupdate.cpp
+++ bin/noteupdate.cpp
@@ -7,13 +7,34 @@
  */
 
 #include "html.h"
+#include <fstream>
 
 // #define _ENABLE_GIT_UPDATE
 
+void errorMessage() {
+  std::cout << "Usage: noteupdate [notename]" << std::endl;
+}
+
+void errorMessage2() {
+  std::cout << "Error: input existing file name" << std::endl;
+}
+
 int main(int argc, char **argv) {
-  std::string comment = "";
+  if (argc != 2) {
+    errorMessage();
+    return -1;
+  }
+  std::string notename(argv[1]);
+  std::ifstream ifs("../note/" + notename + ".html");
+  if (!ifs.is_open()) {
+    errorMessage2();
+    return -2;
+  }
   HTML::update_html("../note/index_temp.html", "../note/index.html");
   system("cp ../note/index.html ../../sochigusa.bitbucket.org/note/index.html");
+  system(
+      ("cp ../note/" + notename + ".html ../../sochigusa.bitbucket.org/note/")
+          .c_str());
 #ifdef _ENABLE_GIT_UPDATE
   system("git pull origin master");
   system("cd ../../sochigusa.bitbucket.org/ && git pull origin master");
@@ -21,8 +42,8 @@ int main(int argc, char **argv) {
           "git push origin master")
              .c_str());
   system(("cd ../../sochigusa.bitbucket.org/ && git commit -a -m \"auto commit "
-          "by gitupdate" +
-          comment + "\" && " + "git push origin master")
+          "by noteupdate " +
+          notename + "\" && " + "git push origin master")
              .c_str());
 #endif
   return 0;
